974f028073736a396c3ab1bacbb0f0596629305c
emily <e975208@gmail.com>
2024-12-21 08:13:36 +0000

    lesson8

--- .devcontainer/lesson8/lesson8_3.py
+++ .devcontainer/lesson8/lesson8_3.py
@@ -1,5 +1,6 @@
 from tools import fetch_youbike_data
 import streamlit as st
+import pandas as pd
 
 youbike_data:list[dict] = fetch_youbike_data()
 
@@ -25,17 +26,23 @@ with col2:
                             '可借車輛數':item['sbi'],
                             '可還空位數':item['bemp'],
                             '營業中':item['act'],
-                            'latitute':item['lat'],
-                            'longitute':item['lng']
+                            'latitude':float(item['lat']),
+                            'longitude':float(item['lng'])
                              } for item in filter_list]
     st.dataframe(show_data)
 
 
-# with col2:
-#     filter_data = filter(lambda item:item['sarea'] == selected_sarea,youbike_data)
-#     st.dataframe(filter_data)
+df = pd.DataFrame(show_data)
 
-# #顯示地圖
-# filter_data = list(filter(lambda item:item['sarea'] == selected_sarea,youbike_data))
-# locations = [{'lat': float(item['lat']), 'lon': float(item['lng'])} for item in filter_data]
-# st.map(locations)
+
+st.map(
+    data=df,
+    latitude='latitude',
+    longitude='longitude',
+    color='#FF0000',  # 紅色標記
+    size=15,          # 標記大小
+)
+
+# 在地圖下方顯示站點詳細資訊
+for _, row in df.iterrows():
+    st.text(row['站點'])
